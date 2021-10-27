bdfc5518bce0752ad72a3ae3ae8f9b24ae4e4b86
Kyle-Wi <black1848@naver.com>
2021-10-27 15:21:24 +0900

    20211027 spring mvc advanced lecture start

---
+++ src/test/java/com/springmvc/advanced/AdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
